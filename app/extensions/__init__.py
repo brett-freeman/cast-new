d0bb1d5b5b07fcf78bef43a4b8efee2c8f3b118b
Brett <brettsfreeman@gmail.com>
2014-04-28 03:43:25 +0000

    Added flask-migrate support.

--- app/extensions/__init__.py
+++ app/extensions/__init__.py
@@ -2,9 +2,11 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.moment import Moment
+from flask.ext.migrate import Migrate, MigrateCommand
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 moment = Moment()
+migrate = Migrate(db)
--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 import os
 from app import create_app
 from flask.ext.script import Manager
-from app.extensions import db
+from app.extensions import db, migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 db.init_app(app)
+migrate.init_app(app, db)
 manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
 	manager.run()
