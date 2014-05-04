4103bc9b36945172610b64d583c41f4af7d20a42
port- <brettsfreeman@gmail.com>
2014-05-03 20:11:13 -0400

    Fixed user api links with trailing slash.

--- app/api_1_0/users.py
+++ app/api_1_0/users.py
@@ -2,9 +2,9 @@ from flask import jsonify
 from . import api
 from ..models import User
 
-@api.route('/users', methods=['GET'])
-@api.route('/users/<string:username>', methods=['GET'])
-@api.route('/users/<int:user_id>', methods=['GET'])
+@api.route('/users/', methods=['GET'])
+@api.route('/users/<string:username>/', methods=['GET'])
+@api.route('/users/<int:user_id>/', methods=['GET'])
 def api_fetch_users(username=None, user_id=None):
 	if username:
 		return jsonify( User.query.filter_by(username=username).first_or_404().to_json )
