b3b5129dd55f859d8929f77dddac6c63344f7d7f
port- <brettsfreeman@gmail.com>
2014-05-13 11:52:29 -0400

    Edit user list in admin panel

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,7 +1,11 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import Required
 
 class AnnouncementForm(Form):
 	message = StringField('Message', validators=[Required()])
 	submit = SubmitField('Submit')
+
+class PickUserForm(Form):
+	user = SelectField('User', coerce=int, validators=[Required()])
+	submit = SubmitField('Edit')
--- app/admin/routes.py
+++ app/admin/routes.py
@@ -3,14 +3,21 @@ from flask import render_template, redirect, url_for, flash
 from flask.ext.login import login_required, current_user
 from app.extensions import db
 from .decorators import admin_required
-from .forms import AnnouncementForm
-from ..models import Announcement
+from .forms import AnnouncementForm, PickUserForm
+from ..models import Announcement, User
+
+@admin.before_request
+@admin_required
+def admin_before_request():
+	pass
 
 @admin.route('/', methods=['GET', 'POST'])
 @login_required
-@admin_required
 def index():
 	announcement_form = AnnouncementForm()
+	pick_user_form = PickUserForm()
+	pick_user_form.user.choices = [ (user.id, user.username) for user in User.query.all()]
+	
 	if announcement_form.validate_on_submit():
 		msg = Announcement(message=announcement_form.message.data)
 		db.session.add(msg)
@@ -18,11 +25,14 @@ def index():
 		flash('Announcement created.')
 		return redirect(url_for('admin.index'))
 
-	return render_template('admin/index.html', announcement_form=announcement_form)
+	if pick_user_form.validate_on_submit():
+		user = User.query.get(pick_user_form.user.data)
+		return redirect(url_for('auth.edit_user', username=user.username))
+
+	return render_template('admin/index.html', announcement_form=announcement_form, pick_user_form=pick_user_form)
 
 @admin.route('/announcement/<int:id>/delete')
 @login_required
-@admin_required
 def delete_announcement(id):
 	msg = Announcement.query.get(id)
 	if not msg:
