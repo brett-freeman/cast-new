f2e8370e5adf5ccde538b21b5892bfd6579d371f
port- <brettsfreeman@gmail.com>
2014-05-13 16:26:40 -0400

    Querying of casts by number instead of id

--- app/api_1_0/casts.py
+++ app/api_1_0/casts.py
@@ -3,10 +3,11 @@ from . import api
 from ..models import Cast, Pick
 
 @api.route('/casts/', methods=['GET'])
-@api.route('/casts/<int:id>/', methods=['GET'])
-def api_fetch_casts(id=None):
-	if id:
-		return jsonify( cast=Cast.query.get_or_404(id).to_json, 
-						picks=[ pick.to_json for pick in Pick.query.filter_by(cast_id=id).all() ])
+@api.route('/casts/<int:cast_number>/', methods=['GET'])
+def api_fetch_casts(cast_number=None):
+	if cast_number:
+		cast = Cast.query.filter_by(cast_number=cast_number).first()
+		return jsonify( cast=cast.to_json, 
+					picks=[ pick.to_json for pick in Pick.query.filter(cast.cast_number==cast_number).all() ])
 
 	return jsonify( casts=[ cast.to_json for cast in Cast.query.all() ])
