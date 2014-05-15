c985e98234f57afa1367a2dfdd3167f096f89cd9
port- <brettsfreeman@gmail.com>
2014-05-15 15:52:38 -0400

    conditional in position updating to make queries less expensive.

--- app/api_1_0/picks.py
+++ app/api_1_0/picks.py
@@ -15,14 +15,16 @@ def api_fetch_picks(id=None):
 @api.route('/dj/update_order/<int:cast_number>', methods=['PUT', 'GET'])
 def update_order(cast_number):
 	picks_order = request.get_json()
-	picks = Pick.query.join(Pick.cast).filter(Cast.cast_number==cast_number).order_by(Pick.dj_list_position.desc()).all()
-	for pick in picks:
+	cast = Cast.query.filter_by(cast_number=cast_number).first()
+	for pick in cast.picks:
 		for pick_order in picks_order:
-			if pick.id == pick_order['id']:
+			if pick.id == pick_order['id'] and pick.dj_list_position != pick_order['position']:
 				pick.dj_list_position = pick_order['position']
 				try:
 					db.session.add(pick)
 					db.session.commit()
 				except Exception as e:
 					return 'Yikes %s' % str(e)
-	return jsonify( picks=[pick.to_json for pick in picks] )
+			else:
+				pass
+	return jsonify( picks=[pick.to_json for pick in cast.picks] )
