e7ca527c2c25cade01f7fa304097e12ab1e6a6a1
port- <brettsfreeman@gmail.com>
2014-05-15 14:13:54 -0400

    Testing post instead of put

--- app/static/js/dj.js
+++ app/static/js/dj.js
@@ -23,7 +23,7 @@ djApp.controller('sortableCtrl', ['$scope', '$http', 'orderByFilter', '$statePar
 					position: $scope.picks.indexOf(i) 
 				};
 			});
-			$http.put('../api/dj/update_order/' + $stateParams.castId, pick_order).success(function(data) {
+			$http.post('../api/dj/update_order/' + $stateParams.castId, pick_order).success(function(data) {
 				console.log(data);
 				$scope.picks = orderByFilter(data.picks, ['dj_list_position']);
 			});
